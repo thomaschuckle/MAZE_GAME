801a42549858d93af967f48f71508a13cc3f3e80
Thomas Ha <ivtuan.h@gmail.com>
2024-10-02 13:14:51 -0400

    Initial commit

---
+++ src/ui/UIPrototype.java
@@ -0,0 +1,16 @@
+package ui;
+
+public class UIPrototype {
+	int max_dim[] = {1000, 630};
+	int top_panel_dim[] = {1000, 30};
+	int p_info_dim[] = {400, 225};
+	int chat_dim[] = {400, 300};
+	int buttons_panel_dim[] = {400, 75};
+	int maze_area_dim[] = {600, 600};
+	
+	int logo_dim[] = {300, 50};
+	int maze_dim[] = {400, 400};
+	int pos_dim[] = {55, 55};
+	int arrow_button_dim[] = {15, 15};
+	int action_button_dim[] = {40, 40};
+}
